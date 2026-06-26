830474ddb901d07e4cd45bc94ef8cf07391c6237
Ankur Verma <ankurverma7619@gmail.com>
2026-06-26 15:53:59 +0530

    Initialization of services

---
+++ api-gateway/src/test/java/com/shopease/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopease.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
